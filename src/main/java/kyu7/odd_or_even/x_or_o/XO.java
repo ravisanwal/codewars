37cb2157853b3c403b21c85ee629b1d783434e8e
Ravi Sanwal <ravi.sanwal@simplymeasured.com>
2017-07-26 17:29:15 -0700

    solution to xo

---
+++ src/main/java/kyu7/odd_or_even/x_or_o/XO.java
@@ -0,0 +1,16 @@
+package kyu7.odd_or_even.x_or_o;
+
+public class XO {
+
+    public static boolean getXO(final String str) {
+        return 0 == str
+                .toLowerCase()
+                .chars()
+                .filter(x -> x == 'x' || x == 'o')
+                .reduce(0, (a, b) -> b == 'x' ? a + 1 : a - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(XO.getXO("xo"));
+    }
+}
