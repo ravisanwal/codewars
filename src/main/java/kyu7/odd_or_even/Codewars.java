ebfb8da43ea0020d79c2e1e2085d9b3e0f83d364
Ravi Sanwal <ravi.sanwal@simplymeasured.com>
2017-07-27 17:16:42 -0700

    solution to even odd

--- src/main/java/kyu7/odd_or_even/Codewars.java
+++ src/main/java/kyu7/odd_or_even/Codewars.java
@@ -3,8 +3,8 @@ package kyu7.odd_or_even;
 public class Codewars {
     public static String oddOrEven (final int[] array) {
         int res = 0;
-        for (int i = 0; i < array.length; i++) {
-            res^=array[i];
+        for (final int item : array) {
+            res ^= item;
         }
         return (res & 1) == 0 ? "even" : "odd";
     }
